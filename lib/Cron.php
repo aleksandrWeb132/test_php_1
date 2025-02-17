6905b0b543bda8c41e276748a802d7866212ac62
Александр Тимохин <a.timohin.prog@solo-it.ru>
2025-02-17 22:31:12 +0400

    Реализовал класс для работы с Cron

--- lib/Cron.php
+++ lib/Cron.php
@@ -3,5 +3,25 @@
 namespace lib;
 
 class Cron {
+    function get($url): array {
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $response = curl_exec($ch);
+        curl_close($ch);
 
+        if (curl_errno($ch)) {
+            return [
+                "code" => 404,
+                "message" => "Ошибка cURL",
+                "body" => $ch
+            ];
+        }
+        else {
+            return [
+                "code" => 1,
+                "message" => "Ответ от сервера",
+                "body" => $response
+            ];
+        }
+    }
 }
