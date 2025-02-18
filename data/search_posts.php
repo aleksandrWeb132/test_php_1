b09fa0f2f65b3afdb536cb6d635214bd87badf10
Александр Тимохин <a.timohin.prog@solo-it.ru>
2025-02-18 19:44:53 +0400

    Реализовал страницу для поиска постов по тексту в комментариях.

--- data/search_posts.php
+++ data/search_posts.php
@@ -4,6 +4,20 @@ require_once "../config/config.php";
 
 use lib\Orm;
 
+$data = json_decode(file_get_contents('php://input'), true);
+
+if(!array_key_exists('search', $data)) {
+    die(json_encode([
+        "code" => 40382,
+        "message" => "Ошибка, не удалось найти параметр поиска!",
+        "body" => $data
+    ]));
+}
+
 $connect = new PDO("mysql:host=$host;dbname=$database", $username, $password);
 
 $orm = new Orm($connect, "POSTS");
+
+$search = $orm->findByString($data["search"]);
+
+die(json_encode($search));
