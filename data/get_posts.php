6f3936b5bb669fa4c51fd6d77978ef891b0e1d64
Александр Тимохин <a.timohin.prog@solo-it.ru>
2025-02-18 13:34:06 +0400

    Создал страницу для получения всех постов.

---
+++ data/get_posts.php
@@ -0,0 +1,13 @@
+<?php
+require_once "autoload.php";
+require_once "../config/config.php";
+
+use lib\Orm;
+
+$connect = new PDO("mysql:host=$host;dbname=$database", $username, $password);
+
+$orm = new Orm($connect, "POSTS");
+
+$posts = $orm->getList();
+
+die($posts);
